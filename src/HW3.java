f447c0149bdbec772b1afedf79fa41a83ba6c812
Azerty <nik.k.2018@mail.ru>
2025-06-09 22:57:14 +0300

    задача 1

---
+++ src/HW3.java
@@ -0,0 +1,17 @@
+public class HW3 {
+    public static void main(String[] args) {
+        int myInt = 42;
+        byte myByte = 10;
+        short myShort = 1000;
+        long myLong = 1234567890L;
+        float myFloat = 3.14f;
+        double myDouble = 2.718281828459045;
+
+        System.out.println("Значение переменной myInt с типом int равно " + myInt);
+        System.out.println("Значение переменной myByte с типом byte равно " + myByte);
+        System.out.println("Значение переменной myShort с типом short равно " + myShort);
+        System.out.println("Значение переменной myLong с типом long равно " + myLong);
+        System.out.println("Значение переменной myFloat с типом float равно " + myFloat);
+        System.out.println("Значение переменной myDouble с типом double равно " + myDouble);
+    }
+}
