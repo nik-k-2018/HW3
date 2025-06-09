4aa5cc2b0371b0fff0f6360b7c55f6a184b6c56b
Azerty <nik.k.2018@mail.ru>
2025-06-09 23:19:19 +0300

    задача 2

---
+++ src/task2.java
@@ -0,0 +1,20 @@
+public class task2 {
+    public static void main(String[] args) {
+        double value1 = 27.12;
+        long value2 = 987678965549L;
+        double value3 = 2.786;
+        short value4 = 569;
+        byte value5 = 100;
+        int value6 = 27897;
+        byte value7 = 67;
+
+        // Выводим значения в консоль
+        System.out.println("value1 = " + value1);
+        System.out.println("value2 = " + value2);
+        System.out.println("value3 = " + value3);
+        System.out.println("value4 = " + value4);
+        System.out.println("value5 = " + value5);
+        System.out.println("value6 = " + value6);
+        System.out.println("value7 = " + value7);
+    }
+}
